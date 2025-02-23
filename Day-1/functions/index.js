252c0fabb6686d1d309cb8ebd3cf5522ce2cdaba
iamKunalBhalerao <kunalbhalerao789@gmail.com>
2025-02-23 23:07:47 +0530

    Completed Topics: Functions in JavaScripts

---
+++ Day-1/functions/index.js
@@ -0,0 +1,23 @@
+// Function in JavaScript
+// function Definition
+function sum(a, b) {
+  return a + b;
+}
+
+// function call
+// let ans = sum(10, 20);
+// console.log(ans);
+
+// Another way to write functions
+
+const newfn = function () {
+  console.log("This si function");
+};
+// newfn();
+
+// OR
+// Arrow funtions
+const newfn2 = () => {
+  console.log("This is another type to write functions");
+};
+// newfn2();
