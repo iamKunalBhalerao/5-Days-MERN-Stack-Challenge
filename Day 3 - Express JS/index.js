1c4e2fd7e5e05e848d8a66cce52a902585246a15
iamKunalBhalerao <kunalbhalerao789@gmail.com>
2025-03-01 15:22:48 +0530

    Tested al the main routes such as get, post, delete. we not use put or patch route because we are not dealing with database for now

--- Day 3 - Express JS/index.js
+++ Day 3 - Express JS/index.js
@@ -2,8 +2,47 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+const USERS = [];
+
+// get data of all users usign get route
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.status(200).json({
+    AllUsers: USERS,
+  });
+});
+
+// create user using post route
+app.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  try {
+    USERS.push({
+      username,
+      password,
+    });
+    res.status(200).json({
+      mesaage: "User is Created",
+    });
+    console.log(USERS);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.delete("/delete", (req, res) => {
+  try {
+    USERS.pop();
+    res.status(200).json({
+      message: "Last User Deleted Successfully",
+    });
+  } catch (e) {
+    res.status(403).json({
+      message: "Something went worng",
+    });
+  }
 });
 
 app.listen(3000, () => {
