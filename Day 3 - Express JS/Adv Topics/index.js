b161517184e2982a5e38627acbf6c9c53201bc83
iamKunalBhalerao <kunalbhalerao789@gmail.com>
2025-03-02 14:03:32 +0530

    Completed some Advanced Topics

---
+++ Day 3 - Express JS/Adv Topics/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+/*
+// express.urlencoded (Parsing URL-encoded Form Data)
+app.use(express.urlencoded({ extended: true }));
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.send("Form Body Recived!");
+});
+app.get("/form", (req, res) => {
+  res.send(`
+      <form method="POST" action="/submit">
+        <input type="text" name="username" placeholder="Username">
+        <input type="email" name="email" placeholder="Email">
+        <button type="submit">Submit</button>
+      </form>
+    `);
+});
+*/
+
+/*
+// express.json (Parsing JSON Data)
+app.post("/api/post", (req, res) => {
+  console.log(req.body);
+  res.status(200).json({
+    message: "JSON Data Recived!",
+    data: req.body,
+  });
+});
+*/
+
+app.listen(3000, () => {
+  console.log("server is on PORT:3000");
+});
