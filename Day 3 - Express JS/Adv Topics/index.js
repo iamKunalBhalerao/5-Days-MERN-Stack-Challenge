dc328ded655b341e041ea961d110e7b5a7883cb5
iamKunalBhalerao <kunalbhalerao789@gmail.com>
2025-03-02 14:16:58 +0530

    Completed Cors in Depth

--- Day 3 - Express JS/Adv Topics/index.js
+++ Day 3 - Express JS/Adv Topics/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
@@ -31,6 +32,16 @@ app.post("/api/post", (req, res) => {
   });
 });
 */
+
+// CORS in Depth
+
+app.get("/data", (req, res) => {
+  res.status(200).json({
+    message: "Message Data From the API!",
+  });
+});
+
+app.use(cors());
 
 app.listen(3000, () => {
   console.log("server is on PORT:3000");
